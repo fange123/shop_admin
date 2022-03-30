6a224dcbb5cb1a416dec562c1b537dc3abd749d5
张海玉 <zhanghaiyu@bonc.com.cn>
2022-03-30 14:32:35 +0800

    feat:scoped属性原理解释

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -24,6 +24,7 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-/* @import url(); 引入css类 */
+// * scoped原理：scoped会给当前组件里面的所有的元素都添加一个随机的自定义属性，比如长这样：data-v-5fhf747
+// * 并且给当前组件的style中的所有样式都加上一个属性选择器，属性选择器和添加的自定义属性是匹配的
 
 </style>

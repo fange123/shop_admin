e74bc5e9e79fc10c6e9f993b8b13e24c0240bb87
张海玉 <zhanghaiyu@bonc.com.cn>
2022-03-30 15:31:14 +0800

    feat:header布局

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,8 +1,20 @@
 <template>
-<div class="">
-  首页
-
-</div>
+     <el-container class="index">
+      <el-header>
+        <div class="logo"></div>
+        <div class="title">
+          <h1>电商管理系统</h1>
+        </div>
+        <div class="logout">
+          Hi~xxx
+          <a href="javascript:;" @click="logout">登出</a>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -20,11 +32,66 @@ export default {
     //生命周期 - 挂载完成（访问DOM元素）
     mounted() {
 
+    },
+
+    methods:{
+      logout() {
+        this.$confirm('是否退出登录?', '温馨提示', {
+          type: 'warning'
+        }).then(() => {
+          localStorage.removeItem('token');
+          this.$router.push('/login');
+          this.$message.success({ message: '登出成功!' });
+
+        }).catch(() => {
+          this.$message('已取消退出');
+        });
+      }
     }
 };
 </script>
 <style lang="less" scoped>
 // * scoped原理：scoped会给当前组件里面的所有的元素都添加一个随机的自定义属性，比如长这样：data-v-5fhf747
 // * 并且给当前组件的style中的所有样式都加上一个属性选择器，属性选择器和添加的自定义属性是匹配的
+.index {
+  height:100%;
+  .el-header {
+    background-color:#bdc3c7;
+    display: flex;
+    height:60px;
+    line-height: 60px;
+    .logo {
+      width:180px;
+      background: url('../assets/images/logo.png') no-repeat center center / contain;
+
+    }
+    .title {
+      flex:1;
+      h1 {
+        text-align: center;
+        color:#fff
+      }
+
+    }
+    .logout {
+      width:180px;
+      text-align: right;
+      a {
+        text-decoration: none;
+        color:#f39c12;
+      }
+
+
+    }
+  }
+  .el-aside {
+    background-color: #7f8c8d;
+  }
+  .el-main {
+    background-color: #ecf0f1;
+  }
+
+
+}
 
 </style>

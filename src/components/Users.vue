b3a98e0b3b7da4582cee8007ca482c3ed430ba74
张海玉 <zhanghaiyu@bonc.com.cn>
2022-03-31 17:48:56 +0800

    feat:用户列表表格渲染

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -3,6 +3,45 @@
     <!-- 面包屑 -->
     <!-- 搜索框 -->
     <!-- 表格 -->
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column
+        prop="username"
+        label="用户名">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="手机号码">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱地址">
+      </el-table-column>
+      <el-table-column
+        label="用户状态">
+        <!-- TODO:默认插槽，新写法 -->
+         <template v-slot:default="scope">
+           <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949">
+          </el-switch>
+         </template>
+
+      </el-table-column>
+      <el-table-column
+        prop="create_time"
+        label="创建时间">
+      </el-table-column>
+      <el-table-column label="操作" width="300">
+         <template v-slot:default="scope">
+           <el-button type="primary" plain icon="el-icon-edit" circle size="small"></el-button>
+           <el-button type="danger" plain icon="el-icon-delete" circle size="small"></el-button>
+            <el-button type="success" plain icon="el-icon-check" round size="small">分配角色</el-button>
+
+
+         </template>
+
+      </el-table-column>
+
+    </el-table>
+
     <!-- 分页 -->
   </div>
 </template>
@@ -15,7 +54,8 @@ export default {
         return {
           query:'',
           pageNum:1,
-          pageSize:5
+          pageSize:5,
+          tableData:[]
         };
     },
     //生命周期 - 创建完成（访问当前this实例）
@@ -37,7 +77,10 @@ export default {
             Authorization:localStorage.getItem('token')
           }
         }).then(res=> {
-          console.log(res.data);
+          const {data,meta:{status}} = res.data;
+          if(status === 200){
+            this.tableData = data.users;
+          }
 
         });
 

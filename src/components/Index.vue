bc15e787fbe5e321bcf27490892c0cd980d26d5f
张海玉 <zhanghaiyu@bonc.com.cn>
2022-03-31 16:05:36 +0800

    feat:侧边栏导航布局

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -11,7 +11,42 @@
         </div>
       </el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+          <el-menu
+            default-active="2"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            unique-opened
+          >
+          <el-submenu index="1">
+            <template v-slot:title>
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">用户列表</span>
+            </el-menu-item>
+
+          </el-submenu>
+          <el-submenu index="2">
+            <template v-slot:title>
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">权限列表</span>
+            </el-menu-item>
+
+          </el-submenu>
+          </el-menu>
+        </el-aside>
         <el-main>Main</el-main>
       </el-container>
     </el-container>

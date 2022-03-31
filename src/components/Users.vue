f795749f0f926542ffa0d90b4d2a343fff278851
张海玉 <zhanghaiyu@bonc.com.cn>
2022-03-31 16:38:42 +0800

    feat:user用户列表发送ajax请求

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="users">
-    用户
+    <!-- 面包屑 -->
+    <!-- 搜索框 -->
+    <!-- 表格 -->
+    <!-- 分页 -->
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name:'Users',
     data() {
         return {
-
+          query:'',
+          pageNum:1,
+          pageSize:5
         };
     },
     //生命周期 - 创建完成（访问当前this实例）
     created() {
+      this.getUserList();
+
+    },
+
+    methods: {
+      getUserList(){
+        axios.get('http://localhost:3000/userList',{
+          params: {
+            query:this.query,
+            pageNum:this.pageNum,
+            pageSize:this.pageSize,
+          },
+          //* 请求必须带token
+          headers:{
+            Authorization:localStorage.getItem('token')
+          }
+        }).then(res=> {
+          console.log(res.data);
+
+        });
+
+      }
 
     },
     //生命周期 - 挂载完成（访问DOM元素）

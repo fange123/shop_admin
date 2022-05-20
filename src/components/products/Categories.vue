82f51c75dc0d93a37363455987e4c775e4bd2048
fange123 <2030564240@qq.com>
2022-05-20 10:55:36 +0800

    feat:商品分类-添加功能完成.

--- src/components/products/Categories.vue
+++ src/components/products/Categories.vue
@@ -1,7 +1,7 @@
 <!--  -->
 <template>
 <div class="categories">
-  <el-button type="success" plain>添加分类</el-button>
+  <el-button type="success" plain @click="showAddDialog">添加分类</el-button>
   <el-table
       v-loading="loading"
       element-loading-text="拼命加载中~~~~"
@@ -34,6 +34,27 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog title="添加分类" :visible.sync="addVisible" width="40%">
+      <el-form label-width="80px" :model="form"  status-icon :rules="rules" ref="form">
+         <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="form.cat_name" placeholder="请输入分类名"></el-input>
+        </el-form-item>
+         <el-form-item label="父级名称" prop="cat_pid">
+             <el-cascader
+             :props="props"
+             clearable
+              v-model="form.cat_pid"
+              :options="options">
+            </el-cascader>
+        </el-form-item>
+
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAdd">确 定</el-button>
+      </span>
+    </el-dialog>
+
 </div>
 </template>
 
@@ -47,6 +68,23 @@ export default {
           pageNum:10,
           total:1,
           loading:true,
+          addVisible:false,
+          options:[],
+          form:{
+            cat_name:'',
+            cat_level:'',
+            cat_pid:''
+          },
+          rules: {
+            cat_name:[
+              { required: true,message:'分类名称不能为空',trigger:['change','blur']}
+            ]
+          },
+          props:{
+            value:'cat_pid',
+            label:'cat_name',
+            checkStrictly:true
+          }
 
         };
     },
@@ -74,7 +112,33 @@ export default {
 
         },1000);
 
+      },
+      async showAddDialog(){
+        this.addVisible = true;
+        const {data,meta:{status}} = await this.$axios.get('/categoriesType2');
+        if(status === 200){
+          this.options = data;
+        }else {
+          this.$message.error('获取失败');
+        }
+      },
+      async handleAdd(){
+
+        try {
+          await this.$refs.form.validate();
+          //TODO:假装请求成功，关闭弹框，清空form
+          this.addVisible = false;
+          this.$refs.form.resetFields();
+          const {} = await this.$axios.post('/categories/add',{
+          cat_name: this.form.cat_name,
+          //只要最后一级父id
+          cat_pid:this.form.cat_pid[this.form.cat_pid.length -1] || 0,
+          cat_level:this.form.cat_pid.length
+        });
+
+        }catch(e){}
       }
+
     },
     //生命周期 - 挂载完成（访问DOM元素）
     mounted() {
